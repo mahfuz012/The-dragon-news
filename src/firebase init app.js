9d6833e48e0241cf92b0483bbd1dbcdf0b4e0343
mahfuz012 <mahfuzrn012@gmail.com>
2023-04-30 23:38:30 +0600

    proper add

--- src/firebase init app.js
+++ src/firebase init app.js
@@ -1,13 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
+
+
+console.log(import.meta.env.VITE_SOME_KEY);
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDw_LldiyuWgn_lXYeVtIO14VILVx93jyE",
-  authDomain: "the-dragon-news-b01c6.firebaseapp.com",
-  projectId: "the-dragon-news-b01c6",
-  storageBucket: "the-dragon-news-b01c6.appspot.com",
-  messagingSenderId: "1049963118406",
-  appId: "1:1049963118406:web:42a98525d5792bb1652f47"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
